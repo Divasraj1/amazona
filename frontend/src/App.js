8222e1a4177ab746c757deaed7bbc9bbaf62c6e3
Divasraj Vimal <vimaldivas12@gmail.com>
2022-04-24 21:26:46 +0530

    added bootstrap to project and added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,34 @@ import {BrowserRouter,Routes,Route, Link} from 'react-router-dom'
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import {LinkContainer} from 'react-router-bootstrap'
 function App() {
   return (
     <BrowserRouter>
-    <div>
+    <div className='d-flex flex-column site-container'>
       <header>
-        <Link to='/'>amazona</Link>  
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>amazona</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
+        
       </header>
       <main>
-        <Routes>
-          <Route path='/products/:slug' element={<ProductScreen/>}/>
-          <Route path='/' element={<HomeScreen/>} />
-        </Routes>
+        <Container>
+          <Routes>
+            <Route path='/products/:slug' element={<ProductScreen/>}/>
+            <Route path='/' element={<HomeScreen/>} />
+          </Routes>
+        </Container>
       </main>
+      <footer className='text-center'>
+        All rights reserved
+      </footer>
     </div>
     </BrowserRouter>
   );
