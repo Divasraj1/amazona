b32d9569fd7d0c53d20aeb711f3ef151e9bd19e3
Divasraj Vimal <vimaldivas12@gmail.com>
2022-04-24 11:52:17 +0530

    featured section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -8,7 +9,25 @@ function App() {
         <a href='/'>amazona</a>  
       </header>
       <main>
-        list products
+        <h1>Featured Products</h1>
+        <div className='products'>
+          {
+            data.products.map(product => (
+              <div className='product' key={product.slug}>
+                <a href={`/products/${product.slug}`}>
+                  <img src={product.image} alt={product.name}/>
+                </a>
+                <div className='product-info'>
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p><strong>${product.price}</strong></p>
+                  <button>Add to Cart</button>
+                </div>
+              </div>
+            ))
+          }
+        </div>
       </main>
     </div>
   );
