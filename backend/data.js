67ce2b9f133add9738377759ead4d9465cb4dc13
Divasraj Vimal <vimaldivas12@gmail.com>
2022-04-24 17:22:55 +0530

    backend api for products

---
+++ backend/data.js
@@ -0,0 +1,53 @@
+const data = {
+    products:[
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg',  //679px X 829px
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirts from NIKE'
+        },
+        {
+            name: 'Adidas Fit Shirt',
+            slug: 'adidas-fit-shirt',
+            category: 'Shirts',
+            image:'/images/p2.jpg',
+            price: 420,
+            countInStock: 30,
+            brand: 'Adidas',
+            rating: 4.6,
+            numReviews: 17,
+            description: 'high quality shirts from ADIDAS'
+        },
+        {
+            name: 'Nike Slim Pants',
+            slug: 'nike-slim-pants',
+            category: 'Pants',
+            image:'/images/p3.jpg',
+            price: 220,
+            countInStock: 17,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 60,
+            description: 'high quality Pants from NIKE'
+        },
+        {
+            name: 'Adidas Slim Pants',
+            slug: 'adidas-slim-pants',
+            category: 'Pants',
+            image:'/images/p4.jpg',
+            price: 250,
+            countInStock: 18,
+            brand: 'Adidas',
+            rating: 4.2,
+            numReviews: 62,
+            description: 'high quality Pants from Adidas'
+        },
+    ]
+}
+export default data;
